9b6ab5283ca7c3e69c48220db379bd7ebb6b953f
Aaron Gussman <agussman@gmail.com>
2018-04-21 19:59:23 -0500

    Tiered select working, but can't select the group level

--- fit-convert-ng5/src/app/app.module.ts
+++ fit-convert-ng5/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule}  from '@angular/material/select';
 import { HttpClientModule } from "@angular/common/http";;
 import { AppComponent } from './app.component';
 import { MatButtonModule, MatMenuModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { FileDropModule } from 'ngx-file-drop';
 
@@ -17,12 +19,14 @@ import { ActivityServiceService } from './activity-service.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     FileDropModule,
     MatButtonModule, 
-    MatMenuModule
+    MatMenuModule,
+    MatSelectModule
   ],
   providers: [ActivityServiceService],
   bootstrap: [AppComponent]
