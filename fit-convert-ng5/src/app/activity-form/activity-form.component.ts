c236094382c6b943320c55d7c6b16e8b4752928b
Aaron Gussman <agussman@gmail.com>
2018-04-23 23:59:13 -0400

    Indent

--- fit-convert-ng5/src/app/activity-form/activity-form.component.ts
+++ fit-convert-ng5/src/app/activity-form/activity-form.component.ts
@@ -40,47 +40,16 @@ export class ActivityFormComponent implements OnInit {
     'main2': ['sub1', 'sub2', 'sub3'],
   };
   
-  activityGroups = [
-    { name: "Run", level: 0 },
-    { name: "Indoor Run", level: 1},
-    { name: "Walk", level: 0}
-  ]
-
-  pokemonGroups = [
-    {
-      name: 'Grass',
-      pokemon: [
-        { value: 'bulbasaur-0', viewValue: 'Bulbasaur' },
-        { value: 'oddish-1', viewValue: 'Oddish' },
-        { value: 'bellsprout-2', viewValue: 'Bellsprout' }
-      ]
-    },
-    {
-      name: 'Water',
-      pokemon: [
-        { value: 'squirtle-3', viewValue: 'Squirtle' },
-        { value: 'psyduck-4', viewValue: 'Psyduck' },
-        { value: 'horsea-5', viewValue: 'Horsea' }
-      ]
-    },
-    {
-      name: 'Fire',
-      disabled: true,
-      pokemon: [
-        { value: 'charmander-6', viewValue: 'Charmander' },
-        { value: 'vulpix-7', viewValue: 'Vulpix' },
-        { value: 'flareon-8', viewValue: 'Flareon' }
-      ]
-    },
-    {
-      name: 'Psychic',
-      pokemon: [
-        { value: 'mew-9', viewValue: 'Mew' },
-        { value: 'mewtwo-10', viewValue: 'Mewtwo' },
-      ]
-    }
+  foods = [
+    {value: 'Run', viewValue: 'Run', indent:0},
+    {value: 'Indoor Run', viewValue: 'Indoor Run', indent:1},
+    {value: 'Walk', viewValue: 'Walk', indent:0}
   ];
 
+  getIndent(indent) {
+    return indent*10;
+  }
+  
   @ViewChild('myInput')
   myFileInput: any;
 
